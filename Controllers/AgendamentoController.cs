6be1b73d959fb029de70a09743c44b2dea225d86
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-07-20 19:27:03 -0300

    Initial project

---
+++ Controllers/AgendamentoController.cs
@@ -0,0 +1,42 @@
+﻿using Consultorio.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Consultorio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgendamentoController : ControllerBase
+    {
+        public List<Agendamento> Agendamentos = new();
+
+        public AgendamentoController()
+        {
+            Agendamentos.Add(new Agendamento
+            {
+                Id = 1,
+                NomePaciente = "Mario Leston Rey",
+                Horario = new DateTime(2022, 07, 20)
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Agendamentos);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            Agendamento agendamento = Agendamentos.FirstOrDefault(x => x.Id == id);
+
+            if (agendamento is null)
+                return BadRequest("Agendamento não encontrado!");
+
+            return Ok(agendamento);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Consultorio
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddCors();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Consultorio", Version = "v1" });
+            });
+        }
+
+       
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Consultorio v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
