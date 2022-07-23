8160dfa1d4e91ae3922c7fba8f6c1d6787e153ac
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-07-23 16:04:32 -0300

    Delete controller + alter consultoriodbcontext config

--- Context/ConsultorioDbContext.cs
+++ Context/ConsultorioDbContext.cs
@@ -5,10 +5,20 @@ namespace Consultorio.Context
 {
     public class ConsultorioDbContext : DbContext
     {
-        public DbSet<Agendamento> Agendamentos { get; set; }
+        public DbSet<Consulta> Consultas {get; set;}
+        public DbSet<Especialidade> Especialidades { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Profissional> Profissionais { get; set; }
+        //public DbSet<ProfissionalEspecialidade> ProfissionaisEspecialidades { get; set; }
 
         public ConsultorioDbContext(DbContextOptions<ConsultorioDbContext> opt) : base(opt)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
         }
     }
 }
