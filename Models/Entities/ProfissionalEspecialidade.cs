d4916ba1fcaa64efc347af2d2e1bba23bcf36df2
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-07-23 19:20:07 -0300

    Removing base in ProfissinalEspecialidade + remove comments

--- Models/Entities/ProfissionalEspecialidade.cs
+++ Models/Entities/ProfissionalEspecialidade.cs
@@ -1,6 +1,6 @@
 namespace Consultorio.Models.Entities
 {
-   public class ProfissionalEspecialidade : Base
+   public class ProfissionalEspecialidade
    {
         public int ProfissionalId { get; set; }
         public Profissional Profissionais { get; set; }
