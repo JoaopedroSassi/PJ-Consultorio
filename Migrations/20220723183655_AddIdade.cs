64f35e0522fdbca44554c44003a64527190dab4c
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-07-23 15:40:24 -0300

    Add migration + add idade

---
+++ Migrations/20220723183655_AddIdade.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Consultorio.Migrations
+{
+    public partial class AddIdade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Idade",
+                table: "Agendamentos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Idade",
+                table: "Agendamentos");
+        }
+    }
+}
--- Models/Entities/Agendamento.cs
+++ Models/Entities/Agendamento.cs
@@ -6,6 +6,7 @@ namespace Consultorio.Models.Entities
     {
         public int Id { get; set; }
         public string NomePaciente { get; set; }
+        public int Idade { get; set; }
         public DateTime Horario { get; set; }
     }
 }
