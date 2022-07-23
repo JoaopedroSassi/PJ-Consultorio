481ed7a7d2158e9f5ab4b67876485eba6291ea56
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-07-23 18:37:47 -0300

    Update properties

--- Maps/ConsultaMap.cs
+++ Maps/ConsultaMap.cs
@@ -17,6 +17,15 @@ namespace Consultorio.Maps
             builder.Property(x => x.Status).HasDefaultValue(1);
             builder.Property(x => x.Preco).HasPrecision(7, 2);
             builder.Property(x => x.DataHorario).IsRequired();
+
+            builder.Property(x => x.PacienteId).IsRequired();
+            builder.HasOne(x => x.Paciente).WithMany(x => x.Consultas).HasForeignKey(x => x.PacienteId);
+
+            builder.Property(x => x.EspecialidadeId).IsRequired();
+            builder.HasOne(x => x.Especialidade).WithMany(x => x.Consultas).HasForeignKey(x => x.EspecialidadeId);
+
+            builder.Property(x => x.ProfissionalId).IsRequired();
+            builder.HasOne(x => x.Profissional).WithMany(x => x.Consultas).HasForeignKey(x => x.ProfissionalId);
         }
    }
 }
