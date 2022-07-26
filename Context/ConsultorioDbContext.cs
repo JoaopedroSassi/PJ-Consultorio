36e45d2fe21f3979db3ea4c23738ffbaa2fec954
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-07-26 18:13:41 -0300

    Migrations

--- Context/ConsultorioDbContext.cs
+++ Context/ConsultorioDbContext.cs
@@ -3,22 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Consultorio.Context
 {
-    public class ConsultorioDbContext : DbContext
-    {
-        public DbSet<Consulta> Consultas {get; set;}
-        public DbSet<Especialidade> Especialidades { get; set; }
-        public DbSet<Paciente> Pacientes { get; set; }
-        public DbSet<Profissional> Profissionais { get; set; }
-        //public DbSet<ProfissionalEspecialidade> ProfissionaisEspecialidades { get; set; }
+	public class ConsultorioDbContext : DbContext
+	{
+		public DbSet<Consulta> Consultas { get; set; }
+		public DbSet<Especialidade> Especialidades { get; set; }
+		public DbSet<Paciente> Pacientes { get; set; }
+		public DbSet<Profissional> Profissionais { get; set; }
+		public DbSet<ProfissionalEspecialidade> ProfissionaisEspecialidades { get; set; }
 
-        public ConsultorioDbContext(DbContextOptions<ConsultorioDbContext> opt) : base(opt)
-        {
-        }
+		public ConsultorioDbContext(DbContextOptions<ConsultorioDbContext> opt) : base(opt)
+		{
+		}
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
-        }
-    }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+		}
+	}
 }
