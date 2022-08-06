b4de7e2d9a5f17f7e4418a2d0a301ded052fce06
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-08-06 19:01:03 -0300

    Profissionalrepository + Iprofissionalrepository

---
+++ Repository/Interfaces/IProfissionalRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Consultorio.Models.Dto;
+using Consultorio.Models.Entities;
+
+namespace Consultorio.Repository.Interfaces
+{
+	public interface IProfissionalRepository : IBaseRepository
+	{
+		Task<IEnumerable<ProfissionalDto>> GetAllProfissionaisAsync();
+		Task<Profissional> GetProfissionalByIdAsync(int id);
+	}
+}
---
+++ Repository/ProfissionalRepository.cs
@@ -0,0 +1,36 @@
+﻿using Consultorio.Context;
+using Consultorio.Models.Dto;
+using Consultorio.Models.Entities;
+using Consultorio.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consultorio.Repository
+{
+	public class ProfissionalRepository : BaseRepository, IProfissionalRepository
+	{
+		private readonly ConsultorioDbContext _context;
+
+		public ProfissionalRepository(ConsultorioDbContext context) : base(context)
+		{
+			_context = context;
+		}
+
+		public async Task<IEnumerable<ProfissionalDto>> GetAllProfissionaisAsync()
+			=> await _context
+						.Profissionais
+						.AsNoTracking()
+						.Select(x => new ProfissionalDto(x.Id, x.Nome, x.Ativo))
+						.ToListAsync();
+
+		public async Task<Profissional> GetProfissionalByIdAsync(int id)
+			=> await _context
+						.Profissionais
+						.AsNoTracking()
+						.Include(x => x.Consultas)
+						.Include(x => x.Especialidades)
+						.FirstOrDefaultAsync(x => x.Id == id);
+	}
+}
