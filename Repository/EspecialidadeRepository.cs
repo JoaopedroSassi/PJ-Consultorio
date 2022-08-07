1c9b9b9bf48834a211eba6986148f4a227542c96
João Pedro Sassi Granado <joaopedro10813@gmail.com>
2022-08-07 19:01:52 -0300

    Add IEspecialidadeRep + especialdiaderep full

---
+++ Repository/EspecialidadeRepository.cs
@@ -0,0 +1,35 @@
+﻿using Consultorio.Context;
+using Consultorio.Models.Dto;
+using Consultorio.Models.Entities;
+using Consultorio.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consultorio.Repository
+{
+	public class EspecialidadeRepository : BaseRepository, IEspecialidadeRepository
+	{
+		private readonly ConsultorioDbContext _context;
+
+		public EspecialidadeRepository(ConsultorioDbContext context) : base(context)
+		{
+			_context = context;
+		}
+
+		public async Task<IEnumerable<EspecialidadeDto>> GetAllEspecialidadesAsync()
+			=> await _context
+					 .Especialidades
+					 .AsNoTracking()
+					 .Select(x => new EspecialidadeDto(x.Id, x.Nome, x.Ativa))
+					 .ToListAsync();
+
+		public async Task<Especialidade> GetEspecialidadeByIdAsync(int id)
+			=> await _context
+						.Especialidades
+						.AsNoTracking()
+						.Include(x => x.Profissionais)
+						.FirstOrDefaultAsync(x => x.Id == id);
+	}
+}
